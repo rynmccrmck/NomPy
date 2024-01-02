c5c80bc35fe9760a35d14eb6763fd2fda9a5501d
rynmccrmck <ryanjmccormack@gmail.com>
2024-01-02 17:19:57 -0500

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nompy',
-    version='0.0.1',
+    version='0.0.2',
     author='Ryan McCormack',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
